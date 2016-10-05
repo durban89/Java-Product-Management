f6d9dde2808fe47fe6ca241975dda634d1c61176
dapeng126 <zhangdapeng89@126.com>
2016-10-05 18:25:35 +0800

    添加 注册service

---
+++ src/main/java/com/produt/dbutil/register/service/RegisterService.java
@@ -0,0 +1,10 @@
+package com.produt.dbutil.register.service;
+
+import java.util.List;
+
+/**
+ * Created by durban126 on 16/10/5.
+ */
+public interface RegisterService {
+    public boolean ResigterUser(List<Object> params);
+}
