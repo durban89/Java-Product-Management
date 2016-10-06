5d014d0d6d83a942d389fae03299ccfbc1224e62
dapeng126 <zhangdapeng89@126.com>
2016-10-06 13:17:19 +0800

    添加产品的添加函数

---
+++ src/main/java/com/produt/dbutil/product/action/ProductAction.java
@@ -0,0 +1,96 @@
+package com.produt.dbutil.product.action;
+
+import com.produt.dbutil.product.dao.ProductDao;
+import com.produt.dbutil.product.service.ProductService;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by durban126 on 16/10/6.
+ */
+public class ProductAction extends HttpServlet {
+    private ProductService service;
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        resp.setContentType("text/html;charset=utf-8");
+        resp.setCharacterEncoding("utf-8");
+        req.setCharacterEncoding("utf-8");
+        String actionFlag = req.getParameter("action_flag");
+        if (actionFlag.equals("add")) {
+            addProduct(req, resp);
+        }
+    }
+
+    private void addProduct(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        //表单含有文件要处理
+        String path = req.getContextPath();
+        DiskFileItemFactory diskFileItemFactory = new DiskFileItemFactory();
+
+        //构建一个文件上传类
+        ServletFileUpload servletFileUpload = new ServletFileUpload(diskFileItemFactory);
+        servletFileUpload.setFileSizeMax(3 * 1024 * 1024);
+        servletFileUpload.setSizeMax(6 * 1024 * 1024);
+
+        List<FileItem> list = null;
+        List<Object> params = new ArrayList<Object>();
+        try {
+            list = servletFileUpload.parseRequest(req);
+            //取出所有表单的值：判断非文本字段和文本字段
+            for (FileItem fileItem : list) {
+                if (fileItem.isFormField()) {
+                    if (fileItem.getFieldName().equals("proname")) {
+                        params.add(fileItem.getString("utf-8"));
+                    }
+
+                    if (fileItem.getFieldName().equals("proprice")) {
+                        params.add(fileItem.getString("utf-8"));
+                    }
+
+                    if (fileItem.getFieldName().equals("proaddress")) {
+                        params.add(fileItem.getString("utf-8"));
+                    }
+                } else {
+                    String image = fileItem.getName();
+                    params.add(image);
+                    String uploadPath = req.getRealPath("/upload");
+                    File realPath = new File(uploadPath + "/" + image);
+//                    if(!realPath.exists()){
+//                        realPath.createNewFile();
+//                    }
+                    fileItem.write(realPath);
+
+                    //把数据插入数据库
+                    boolean flag = service.addProduct(params);
+                    if (flag) {
+                        resp.sendRedirect(path + "/product/item.jsp");
+                    }
+                }
+            }
+
+        } catch (FileUploadException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        service = new ProductDao();
+    }
+}
