494c86bb0451e06bfbe67a2543fb4490e9514d21
dapeng126 <zhangdapeng89@126.com>
2016-10-06 13:17:45 +0800

    添加产品的Service

---
+++ src/main/java/com/produt/dbutil/product/service/ProductService.java
@@ -0,0 +1,10 @@
+package com.produt.dbutil.product.service;
+
+import java.util.List;
+
+/**
+ * Created by durban126 on 16/10/6.
+ */
+public interface ProductService {
+    public boolean addProduct(List<Object> params);
+}
